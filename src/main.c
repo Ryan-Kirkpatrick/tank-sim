4e133f4f2f631cb7eb496d065cce61664a217633
Ryan Kirkpatrick <ryan.kptk@outlook.com>
2025-08-30 15:56:01 +1000

    .

--- src/main.c
+++ src/main.c
@@ -1,37 +1,123 @@
 #include "FreeRTOS.h"
+#include "class/hid/hid_device.h"
+#include "device/usbd.h"
 #include "hardware/exception.h"
+#include "hardware/adc.h"
+#include "portmacro.h"
+#include "projdefs.h"
 #include "task.h"
 
 #include <pico/time.h>
 #include "pico/stdlib.h"
+#include "tusb.h"
 
-#define STACK_SIZE 512
+#define STACK_SIZE 1024 * 8
 
 #define LED_PIN 25
+#define POT_PIN 26
 
-StackType_t stack[STACK_SIZE];
-StaticTask_t task_handle;
+StackType_t led_task_stack[STACK_SIZE];
+StaticTask_t led_task_handle;
 
-void task(void* unused) {
+StackType_t usb_task_stack[STACK_SIZE];
+StaticTask_t usb_task_handle;
+
+StackType_t pot_task_stack[STACK_SIZE];
+StaticTask_t pot_task_handle;
+
+void send_key(uint8_t keycode) {
+    uint8_t report[8] = {0};  // modifiers + reserved + 6 keys
+    report[2] = keycode;      // key press
+    tud_hid_keyboard_report(0, 0, report);
+}
+
+
+const uint32_t max_delay = 500;
+volatile uint32_t delay = max_delay;
+
+void led_task(void* unused) {
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (1) {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(delay));
         gpio_put(LED_PIN, 0);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(delay));
+    }
+}
+
+void pot_task(void* unused) {
+    adc_init();
+    adc_gpio_init(POT_PIN);
+    adc_select_input(0);
+    
+    while (1) {
+        uint16_t raw = adc_read();
+        delay = max_delay - ((raw * max_delay) / 4095);
+        vTaskDelay(pdMS_TO_TICKS(5));
+    }
+}
+
+void usb_task(void* unused) {
+    tusb_init();
+
+    TickType_t next = 0;
+
+    while (1) {
+        tud_task();
+        if (tud_hid_ready()) {
+            // send_key(0x04);
+        }
+        vTaskDelay(pdMS_TO_TICKS(200));
+
+        tud_task();
+        if (tud_hid_ready()) {
+            // send_key(0x00);
+        }
+        vTaskDelay(pdMS_TO_TICKS(200));
     }
 }
 
 int main(void) {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // Init GPIO
 
-    xTaskCreateStatic(task, "", STACK_SIZE, NULL, 1, stack, &task_handle);
+
+    // Start tasks
+
+    xTaskCreateStatic(led_task, "", STACK_SIZE, NULL, 1, led_task_stack, &led_task_handle);
+    xTaskCreateStatic(usb_task, "", STACK_SIZE, NULL, 2, usb_task_stack, &usb_task_handle);
+    xTaskCreateStatic(pot_task, "", STACK_SIZE, NULL, 1, pot_task_stack, &pot_task_handle);
     vTaskStartScheduler();
 
-    // while (1) {
-    //     gpio_put(LED_PIN, 1);
-    //     sleep_ms(1000);
-    //     gpio_put(LED_PIN, 0);
-    //     sleep_ms(1000);
-    // }
 }
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer,
+                           uint16_t bufsize) {
+    // we don’t need to handle this for a basic keyboard
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)bufsize;
+}
+
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer,
+                               uint16_t reqlen) {
+    // we don’t need to provide anything for basic keyboard
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)reqlen;
+
+    return 0;  // return zero bytes
+}
+
+// uint8_t const* tud_hid_descriptor_report_cb(uint8_t instance) {
+//     (void)instance;
+//     static uint8_t const report[] = {TUD_HID_REPORT_DESC_KEYBOARD()};
+//     return report;
+// }
+
+
